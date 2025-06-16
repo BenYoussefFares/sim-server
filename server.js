caecafcd8fe0b989d321c9ee659401a35d1aec8e
BenYoussefFares <163755303+BenYoussefFares@users.noreply.github.com>
2025-06-16 01:53:33 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'POST' && req.url === '/') {
+    let body = "";
+    req.on('data', chunk => (body += chunk));
+    req.on('end', () => {
+      console.log("📩 Données reçues :", body);
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end("OK\n");
+    });
+  } else {
+    res.writeHead(400);
+    res.end("Bad Request");
+  }
+});
+
+server.listen(1880, () => {
+  console.log("🚀 Serveur HTTP en écoute sur le port 1880");
+});
